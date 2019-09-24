baad9e1f8376f4a053736bd0f2e699334ca89755
Mathew May <mathewm@hotmail.co.nz>
2019-09-24 11:26:49 +0800

    lib hack to point to our renderable for testing

--- grade/grading/form/rubric/lib.php
+++ grade/grading/form/rubric/lib.php
@@ -22,6 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+use gradingform_rubric\gradingpanel;
+
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot.'/grade/grading/form/lib.php');
@@ -929,10 +931,11 @@ class gradingform_rubric_instance extends gradingform_instance {
      * @return string
      */
     public function render_grading_element($page, $gradingformelement) {
+        return (new gradingpanel($this, true, $gradingformelement))->build_for_template($page);
         global $USER, $OUTPUT;
         if (!$gradingformelement->_flagFrozen) {
             $module = array('name'=>'gradingform_rubric', 'fullpath'=>'/grade/grading/form/rubric/js/rubric.js');
-            //$page->requires->js_init_call('M.gradingform_rubric.init', array(array('name' => $gradingformelement->getName())), true, $module);
+            $page->requires->js_init_call('M.gradingform_rubric.init', array(array('name' => $gradingformelement->getName())), true, $module);
             $mode = gradingform_rubric_controller::DISPLAY_EVAL;
         } else {
             if ($gradingformelement->_persistantFreeze) {
@@ -976,7 +979,6 @@ class gradingform_rubric_instance extends gradingform_instance {
             $html .= html_writer::tag('div', $this->get_controller()->get_formatted_description(), array('class' => 'gradingform_rubric-description'));
         }
         $html .= $this->get_controller()->get_renderer($page)->display_rubric($criteria, $options, $mode, $gradingformelement->getName(), $value);
-        $html .= $OUTPUT->render_from_template('gradingform_rubric/shell', null);
         return $html;
     }
 }
